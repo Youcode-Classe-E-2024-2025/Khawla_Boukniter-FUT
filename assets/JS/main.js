340b7d92a223040ffb8efbeecd01d19add15995a
Khawla Boukniter <boukniter.khawla@gmail.com>
2024-12-01 11:50:13 +0100

    Ajouter la fonctionnalité de réinitialisation de l'équipe

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -348,3 +348,49 @@ function addChangements() {
                 
             });
 }
+
+const resetTeamButton = document.getElementById('reset-team');
+
+resetTeamButton.addEventListener('click', () => {
+    const playerCards = document.querySelectorAll('.selectedPlayer');
+
+    playerCards.forEach(card => {
+        const parentSlot = card.parentElement;
+
+        const img = parentSlot.querySelector('img');
+        if (img) {
+            img.style.opacity = "0";
+
+            setTimeout(() => {
+                img.src = 'https://selimdoyranli.com/cdn/fut-player-card/img/card_bg.png'; // Réinitialiser l'image
+                img.style.opacity = "1"; // Réaugmenter l'opacité
+            }, 500);
+        }
+        
+        parentSlot.innerHTML = `
+                                <div class="bench-con flex flex-wrap gap-3">
+                                    <div class="slot relative text-center h-72 w-36">
+                                        <button class="cardbutton button-reset" aria-label="Card Button">
+                                            <img class="placeholder-img placeholder-enable-hover-shadow" src="https://selimdoyranli.com/cdn/fut-player-card/img/card_bg.png">
+                                            <div class="add-icon absolute left-1/2 top-[40%] flex justify-center items-center" style="translate: -50% -50%">
+                                                <span class="display-contents">
+                                                    <svg class="" viewBox="0 0 36 42" fill="none" width="36">
+                                                        <path d="M18.6275 41.711L18.3137 41.0298C18.1146 41.1215 17.8854 41.1215 17.6863 41.0298L17.3726 41.711L17.6863 41.0298L1.18627 33.4311C0.920355 33.3087 0.75 33.0427 0.75 32.7499V8.7248C0.75 8.42506 0.928458 8.15411 1.20383 8.03575L17.7038 0.943648C17.8929 0.862375 18.1071 0.862375 18.2962 0.943648L34.7962 8.03575C35.0715 8.15411 35.25 8.42506 35.25 8.7248V32.7499C35.25 33.0427 35.0796 33.3087 34.8137 33.4311L18.3137 41.0298L18.6275 41.711Z" stroke="currentColor" stroke-width="1.5"></path>
+                                                    </svg>
+                                                </span>
+                                                <div class="absolute text-3xl">+</div>
+                                            </div>
+                                        </button>
+                                    </div>
+                                </div>
+        `;
+    });
+    
+    document.querySelector('.changements').innerHTML = '';
+
+    changements = [];
+    selectedPlayers = [];
+    
+    alert("L'équipe a été réinitialisée avec succès !");
+});
+
